6e3549f674cf330a6b61741052927197cc53aa87
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-05-30 09:26:20 -0300

    fix: change property svg
    property stroke remove stroke in middle property

---
+++ src/components/Arrow.tsx
@@ -0,0 +1,20 @@
+export const Arrow = () => {
+  return (
+      
+      <svg 
+        className="flex place-self-center"
+        width="10" 
+        height="18" 
+        viewBox="0 0 10 18" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg">
+
+      <path d="M1.53784 1.85718L8.96005 9.00003L1.53784 16.1429" 
+            stroke="white" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"/>
+      </svg>
+      
+  )
+}
